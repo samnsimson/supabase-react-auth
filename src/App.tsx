8377571529da1f808e39f1e1cc21c5d431f350b3
samnsimson <samnishanth01@gmail.com>
2022-11-19 23:50:18 +0530

    inital

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useUserStore } from "./store";
+import { LoginPage, RegisterPage, RootView } from "./views";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { user } = useUserStore();
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: user ? <RootView /> : <LoginPage />,
+        },
+        {
+            path: "/register",
+            element: <RegisterPage />,
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
 }
 
 export default App;
